d28dc6aca46a4c42ebc20c236deee85f6a0ea83b
ThiagoThmaz <thiago.conceicao@sptech.school>
2024-05-08 23:33:38 -0300

    'Fibonacci'

--- Fibonacci/src/test/java/org/example/FibonacciTest.java
+++ Fibonacci/src/test/java/org/example/FibonacciTest.java
@@ -9,37 +9,37 @@ class FibonacciTest {
 
     @Test
     void testar_fibonnaci_termo1() {
-        Assertions.assertEquals(0,Fibonacci.calcularFibonacci(0));
-    }
-
-    @Test
-    void testar_fibonnaci_termo2(){
         Assertions.assertEquals(1,Fibonacci.calcularFibonacci(1));
     }
 
     @Test
-    void testar_fibonnaci_termo3(){
-        Assertions.assertEquals(1, Fibonacci.calcularFibonacci(2));
+    void testar_fibonnaci_termo2(){
+        Assertions.assertEquals(1,Fibonacci.calcularFibonacci(2));
     }
 
     @Test
-    void testar_fibonnaci_termo4(){
+    void testar_fibonnaci_termo3(){
         Assertions.assertEquals(2, Fibonacci.calcularFibonacci(3));
     }
 
     @Test
+    void testar_fibonnaci_termo4(){
+        Assertions.assertEquals(3, Fibonacci.calcularFibonacci(4));
+    }
+
+    @Test
     void testar_fibonnaci_termo5(){
-        Assertions.assertEquals(3, Fibonacci.calcularFibonacci(5));
+        Assertions.assertEquals(5, Fibonacci.calcularFibonacci(5));
     }
 
     @Test
     void testar_fibonnaci_termo6(){
-        Assertions.assertEquals(13, Fibonacci.calcularFibonacci(8));
+        Assertions.assertEquals(8, Fibonacci.calcularFibonacci(6));
     }
 
     @Test
     void testar_fibonnaci_termo7(){
-        Assertions.assertEquals(34, Fibonacci.calcularFibonacci(21));
+        Assertions.assertEquals(13, Fibonacci.calcularFibonacci(7));
     }
 
 }
